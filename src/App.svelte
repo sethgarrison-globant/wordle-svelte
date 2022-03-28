f52b48be454e47a96bc3d1fca9c9b1144522c0da
sethgarrison-globant <66090870+sethgarrison-globant@users.noreply.github.com>
2022-03-28 12:01:27 -0700

    starting place for adding functionality and sveltification

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,155 @@
 <script>
-	export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="game-container">
+	<div class="title-container">
+		<h1>Wordle</h1>
+	</div>
+	<div class="tile-container">
+			<div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+			</div>
+			<div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+			</div>
+			<div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+			</div>
+			<div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+			</div>
+			<div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+			</div>
+			<div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+				<div class="tile"></div>
+			</div>
+	</div>
+
+	<div class="key-container">
+			<button>Q</button>
+			<button>W</button>
+			<button>E</button>
+			<button>R</button>
+			<button>T</button>
+			<button>Y</button>
+			<button>U</button>
+			<button>I</button>
+			<button>O</button>
+			<button>P</button>
+			<button>A</button>
+			<button>S</button>
+			<button>D</button>
+			<button>F</button>
+			<button>G</button>
+			<button>H</button>
+			<button>J</button>
+			<button>K</button>
+			<button>L</button>
+			<button>ENTER</button>
+			<button>Z</button>
+			<button>X</button>
+			<button>C</button>
+			<button>V</button>
+			<button>B</button>
+			<button>N</button>
+			<button>M</button>
+			<button>DELETE</button>
+	</div>
+</div>
 
 <style>
-	main {
+	:global(*) {
+		color: #ffffff;
+		font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande',
+		'Lucida Sans', Arial, sans-serif;
+	}
+
+	:global(body) {
+		background-color: #121213;
+	}
+
+	.game-container {
+		height: 90vh;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.title-container {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 510px;
+		border-bottom: solid 1px #3a3a3c;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	/*TILES*/
+	.tile-container {
+		width: 330px;
+		margin-bottom: 30px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.tile-container div {
+		display: flex;
+	}
+
+	.tile-container .tile {
+		width: 62px;
+		height: 62px;
+		border: 2px solid#3a3a3c;
+		box-sizing: border-box;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 2px;
+	}
+
+	/*	KEYS*/
+	.key-container {
+		width: 510px;
+		display: flex;
+		flex-wrap: wrap;
+	}
+
+	.key-container button {
+		width: 43px;
+		height: 58px;
+		border-radius: 4px;
+		border: none;
+		background-color: #818384;
+		margin: 4px;
+	}
+
+	.key-container button:nth-child(11) {
+		margin-left: 30px;
+	}
+
+	.key-container button:nth-child(20),
+	.key-container button:nth-child(28) {
+		width: 68px;
 	}
 </style>
